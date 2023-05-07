f41630570c860ac5e7e1716b10777e3c3c7621a6
amirti <amirhoseinking82@gmail.com>
2023-05-08 00:09:00 +0330

    project finished

--- example.js
+++ example.js
@@ -1,5 +1,13 @@
 //defining students details in obj
 let students=[{eyeColor:"blue",age:19},{eyeColor:"green",age:20},{eyeColor:"black",age:16}]
-
+//we sould save results in variable
+//sorting array lower to higher
+let result=students.sort((a,b)=>{
+    if(a.age<b.age)return -1;
+    if(b.age<a.age) return 1;
+    else return 0;
+})
+console.log(result);
+//for results open console page(f12) in your browser or node
 
     
